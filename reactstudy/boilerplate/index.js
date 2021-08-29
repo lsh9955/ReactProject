371fd4518488f4ed9f69b42e7105734fae323304
lsh9955 <cqe9933@gmail.com>
2021-08-29 19:51:11 +0900

    nodejs연습중

---
+++ reactstudy/boilerplate/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const { User } = require("./models/User");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://lee:cookierun@youtubeclone.lv19k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
