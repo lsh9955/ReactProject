2f544edd916ee05826da395e3db44e7a98c84271
lsh9955 <cqe9933@gmail.com>
2021-10-09 03:44:27 +0900

    단어 추천

---
+++ recommendword/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import SearchBoard from "./board/SearchBoard";
+
+function App() {
+  return (
+    <>
+      <div className="App">사용자 단어 추천</div>
+      <SearchBoard />
+    </>
+  );
+}
+
+export default App;
---
+++ recommendword/src/board/SearchBoard.js
@@ -0,0 +1,15 @@
+import { React, useState } from "react";
+import ShowWord from "./ShowWord";
+
+const SearchBoard = () => {
+  const [value, setValue] = useState("");
+  return (
+    <>
+      <form>
+        <input onChange={(e) => setValue(e.currentTarget.value)} />
+      </form>
+      <ShowWord nowValue={value} />
+    </>
+  );
+};
+export default SearchBoard;
