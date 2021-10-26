e6da7bcaaa6a5557c9d470ae932cc9c0b1ba6b34
lsh9955 <cqe9933@gmail.com>
2021-10-26 23:02:47 +0900

    userLike 생성

--- recommendword/src/App.js
+++ recommendword/src/App.js
@@ -5,18 +5,21 @@ import title from "../src/board/title.svg";
 import posts from "../src/board/posts.svg";
 import search from "../src/board/search.svg";
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import UserLike from "./board/UserLike";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="titleBlock">
-        <img src={logo} style={{ width: "80px", height: "80px", position: "relative" }} />
-        <img src={title} style={{ width: "200px", height: "100px", position: "relative" }} />
-      </div>
+      <Link to="/">
+        <div className="titleBlock">
+          <img src={logo} style={{ width: "80px", height: "80px", position: "relative" }} />
+          <img src={title} style={{ width: "200px", height: "100px", position: "relative" }} />
+        </div>
+      </Link>
       <hr></hr>
       <Switch>
         <Route exact path="/">
-          <Link to="/posts">
+          <Link to="/post">
             <img src={posts} />
           </Link>
           <Link to="/search">
@@ -28,7 +31,9 @@ function App() {
           <div className="App">사용자 단어 추천</div>
           <SearchBoard />
         </Route>
-        <Route exact path="/post"></Route>
+        <Route exact path="/post">
+          <UserLike />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
--- recommendword/src/board/UserLike.js
+++ recommendword/src/board/UserLike.js
@@ -3,11 +3,19 @@ import { React, useState } from "react";
 const UserLike = () => {
   const [value, setValue] = useState("");
 
+  function clickPlace() {
+    console.log("hell");
+    return <div></div>;
+  }
+  function clickDay() {}
+  function clickBudget() {}
+  function clickBudgetOrder() {}
   return (
     <>
-      <form>
-        <input onChange={(e) => setValue(e.currentTarget.value)} />
-      </form>
+      <div>나는 {<span onClick={clickPlace}>_____</span>}을(를) 가려고 해</div>
+      <div>기간은 {<span onClick={clickDay}>__</span>}일이야</div>
+      <div>예산은 {<span onClick={clickBudget}>____</span>} 이고</div>
+      <div>{<span onClick={clickBudgetOrder}>___</span>}순으로 많이 쓸 거야</div>
     </>
   );
 };
